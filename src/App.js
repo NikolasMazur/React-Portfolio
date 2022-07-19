0ef987a971cb7aafbbf01901db07639b4eda408b
Nikolas <kolemazur@gmail.com>
2022-07-19 17:33:09 -0600

    Create file structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PortfolioContainer from "./components/Project.js";
+
+const App = () => <PortfolioContainer />;
+
+function App() {
+    return(
+        <div>
+            <Header />
+            <Project />
+            <Footer />
+        </div>
+    )
+};
+
+export default App;
