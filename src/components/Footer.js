6c33e4c8ffb84ff2529534acc2176a5de6f933ed
Nikolas <kolemazur@gmail.com>
2022-07-20 18:44:57 -0600

    Add footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 function Footer() {
-    
+    <div id="contact">
+    <h3>Contact</h3>
+    <ul class="footer">
+      <li class="email">Email: kolemazur@gmail.com</li>
+      <li class="links"><a
+      href="https://www.linkedin.com/in/nikolas-mazur-97b312230/" target="_blank"
+      >LinkedIn</a></li>
+      <li class="links"><a
+      href="https://github.com/NikolasMazur" target="_blank"
+      >GitHub</a></li>
+      <li class="links"><a
+      href="./assets/pdf/Resume (2022).pdf" target="_blank"
+      >Resume PDF</a></li>
+    </ul>
+  </div>
 };
 
 export default Footer;
