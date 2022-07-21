3db9c6e487e41d40cb626516f21ada3ee75dd2df
Nikolas <kolemazur@gmail.com>
2022-07-20 18:40:20 -0600

    Add Navigation file/links

---
+++ src/components/Nav.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Nav(currentPage, handlePageChange) {
+    <div>
+    <nav class="links">
+      <ul class="navigation">
+        <li><a 
+              href="#main-project"
+              onClick={() => handlePageChange('Development')}
+              className={currentPage === 'Development' ? 'nav-link active' : 'nav-link'}
+              >Development</a></li>
+        <li><a 
+              href="#contact"
+              onClick={() => handlePageChange('Contact')}
+              className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+              >Contact</a></li>
+        <li><a 
+              href="#resume"
+              onClick={() => handlePageChange('Resume')}
+              className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+              >Resume</a></li>
+        <li><a 
+              href="#about-me"
+              onClick={() => handlePageChange('About')}
+              className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+              >About</a></li>
+      </ul>
+    </nav>
+    </div>
+};
+
+export default Nav;
