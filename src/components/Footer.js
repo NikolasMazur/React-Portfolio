8aeedb5b641bef85b17c94bcbd94c7a9066778f4
Nikolas <kolemazur@gmail.com>
2022-08-04 22:19:47 -0600

    Fix pathing and add local styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,33 @@
 import React from 'react';
 
+const style = {
+  footerFormat: {
+    justifyContent: "center",
+    textAlign: "center",
+    lineHeight: "30px",
+    marginTop: "80px",
+    marginBottom: "30px",
+  },
+  footer: {
+    justifyContent: "center",
+    textAlign: "center",
+    fontSize: "2em",
+  },
+}
+
 function Footer() {
   return(
-    <div id="contact">
+  <div style={style.footerFormat} id="contact">
     <h3>Contact</h3>
-    <ul class="footer">
-      <li class="email">Email: kolemazur@gmail.com</li>
-      <li class="links"><a
+    <ul className="footer">
+      <li className="email">Email: kolemazur@gmail.com</li>
+      <li className="links"><a
       href="https://www.linkedin.com/in/nikolas-mazur-97b312230/" target="_blank"
       >LinkedIn</a></li>
-      <li class="links"><a
+      <li className="links"><a
       href="https://github.com/NikolasMazur" target="_blank"
       >GitHub</a></li>
-      <li class="links"><a
+      <li className="links"><a
       href="./assets/pdf/Resume (2022).pdf" target="_blank"
       >Resume PDF</a></li>
     </ul>
