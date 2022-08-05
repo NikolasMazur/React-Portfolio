2f605edfb7c4591f6e836fb64b345516619b0f44
Nikolas <kolemazur@gmail.com>
2022-08-04 22:18:00 -0600

    Edit Nav component layout

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import Header from "./components/Header";
 import Nav from "./components/Nav"
 import Project from "./components/Project";
 import Footer from "./components/Footer";
+import PortfolioContainer from "./components/PortfolioContainer";
+import About from "./components/pages/About";
 
 // const App = () => <Project />;
 
 function App() {
     return(
         <div>
-            <Header />
             <Nav />
+            <Header />
+            {/* <PortfolioContainer /> */}
             <Project />
+            <About />
             <Footer />
         </div>
     )
