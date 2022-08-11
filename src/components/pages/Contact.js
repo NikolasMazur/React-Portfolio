b97d9a0cfc2c2b46e77d83c67fd33e66a6afc042
Nikolas <kolemazur@gmail.com>
2022-08-11 16:42:03 -0600

    Create contact input fields

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,21 +1,78 @@
+// Currently non-functional as there is no backend
 import React, { useState } from 'react';
 import '../../../src/components/style.css';
 
 import { validateEmail } from '../utils/helpers';
 
-function Contact() {
-    return (
-        <div>
-          <section id="contact">
-            <h3>Contact</h3>
-            <ul class="footer">
-              <li class="email">Email: kolemazur@gmail.com</li>
-              <li class="links"><a href="https://www.linkedin.com/in/nikolas-mazur-97b312230/" target="_blank">LinkedIn</a></li>
-              <li class="links"><a href="https://github.com/NikolasMazur" target="_blank">GitHub</a></li>
-            </ul>
-          </section>
-        </div>
-    )
+const style = {
+  contactFormat: {
+    justifyContent: "center",
+    textAlign: "center",
+    lineHeight: "170%",
+    marginTop: "10%",
+    marginBottom: "10%",
+  },
+  footer: {
+    justifyContent: "center",
+    textAlign: "center",
+    fontSize: "2em",
+  },
+}
+
+
+function Contact(props) {
+  const [input, setInput] = useState("");
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInput("");
+  };
+  const handleChange = (e) => {
+    setInput(e.target.value);
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  return (
+    <section style={style.contactFormat}>
+      <h1 className="section-title">Contact Me</h1>
+      <div className="form-container">
+        <form id="contact-form" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <br />
+            <input
+              type="text"
+              defaultValue={form.name}
+              name="name"
+              onBlur={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <br />
+            <input
+              type="email"
+              defaultValue={form.email}
+              name="email"
+              onBlur={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <br />
+            <textarea
+              name="message"
+              defaultValue={form.message}
+              onBlur={handleChange}
+              rows="5"
+            />
+          </div>
+          <button type="submit" className="contact-button">
+            Add message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
