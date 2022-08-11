254c7d542c11f617a7c2b85f453cccc04b106dc4
Nikolas <kolemazur@gmail.com>
2022-08-11 16:22:35 -0600

    Replace components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header";
-import Nav from "./components/Nav"
-import Project from "./components/Project";
-import Footer from "./components/Footer";
 import PortfolioContainer from "./components/PortfolioContainer";
-import About from "./components/pages/About";
 
 // const App = () => <Project />;
 
 function App() {
     return(
         <div>
-            <Nav />
-            <Header />
-            {/* <PortfolioContainer /> */}
-            <Project />
-            <About />
-            <Footer />
+            <PortfolioContainer />
         </div>
     )
 };
