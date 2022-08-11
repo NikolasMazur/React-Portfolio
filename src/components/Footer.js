3dff0f734561aee91c5e4ad68d254c92534672f1
Nikolas <kolemazur@gmail.com>
2022-08-11 16:25:39 -0600

    Replace Resume link with Stack Overflow link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,18 +18,18 @@ const style = {
 function Footer() {
   return(
   <div style={style.footerFormat} id="contact">
-    <h3>Contact</h3>
+    <h3>Additional Information</h3>
     <ul className="footer">
       <li className="email">Email: kolemazur@gmail.com</li>
-      <li className="links"><a
-      href="https://www.linkedin.com/in/nikolas-mazur-97b312230/" target="_blank"
-      >LinkedIn</a></li>
       <li className="links"><a
       href="https://github.com/NikolasMazur" target="_blank"
       >GitHub</a></li>
       <li className="links"><a
-      href="./assets/pdf/Resume (2022).pdf" target="_blank"
-      >Resume PDF</a></li>
+      href="https://www.linkedin.com/in/nikolas-mazur-97b312230/" target="_blank"
+      >LinkedIn</a></li>
+      <li className="links"><a
+      href="https://stackoverflow.com/users/18041955/nikolas" target="_blank"
+      >Stack Overflow</a></li>
     </ul>
   </div>
   );
