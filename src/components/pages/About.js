470f3f6ce01991185bff18ecba11a2bdd7ccaf94
Nikolas <kolemazur@gmail.com>
2022-08-11 16:42:36 -0600

    Fix photo bug

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import Photo from '../../assets/images/IMG_3673.jpg'
 
 export default function About() {
     return (
         <div>
           <section id="about-me">
-            <h2>About Me</h2>
-            <img class="photo" src="./assets/images/IMG_3674.jpg" alt="Temporary Nikolas picture" />
-            <p class="paragraph">Hi, I'm Nikolas Mazur, a developer currently located in Lander, Wyoming. I've previously been employed by Falcon 
-              Trading Systems as a systems technician. From 2017 to 2019 I was a student at Central Wyoming College studying film, my time there 
-              fostered my love of the visual arts leading to making various short films with friends and previous classmates as a hobby.
-              <br />
-              I've received certifications in varous languages including HTML, CSS, SQL, Python, and MongoDB. Currently I'm enrolled in a full 
+            <img className="photo" src={Photo} alt="Out of date Nikolas picture" />
+            <p className="paragraph"> a developer currently located in Lander, Wyoming. I've received certifications in varous languages including HTML, CSS, SQL, Python, and MongoDB. Currently I'm enrolled in a full 
               stack development bootcamp to further my knowledge of web devlopment languages and technologies; namely HTML, CSS, and JavaScript.
+              <br />
+              From 2021 to 2022 I was employed by Falcon 
+              Trading Systems as a systems technician. From 2017 to 2019 I was a student at Central Wyoming College studying film, my time there 
+              fostered my love of the visual arts leading to making various short films with friends and former classmates.
             </p>
           </section>
         </div>
