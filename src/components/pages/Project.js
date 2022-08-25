63f230c6686b67ad14256868a90a2b5fa66ac9a6
Nikolas <kolemazur@gmail.com>
2022-08-25 17:26:06 -0600

    Reformat HTML

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -15,9 +15,9 @@ function Project() {
               <header className="flex"><a href="https://areitan.github.io/PocketMonster/" target="_blank">PocketMonster</a></header>
                 <a href="https://areitan.github.io/PocketMonster/" target="_blank"><img src={MainProject} alt="PocketMonster landing page." /></a>
                 <h4 className="repository">
-                <a href="https://github.com/areitan/PocketMonster" target="_blank">Repository</a>
-                <br />
-                Created with: HTML, CSS/Tailwind, JavaScript, Open 5E/goQR APIs
+                  <a href="https://github.com/areitan/PocketMonster" target="_blank">Repository</a>
+                  <br />
+                  Created with: HTML, CSS/Tailwind, JavaScript, Open 5E/goQR APIs
                 </h4>
             </section>
             {/* <!-- Project 2 --> */}
@@ -25,29 +25,29 @@ function Project() {
               <header className="flex"><a href="https://nikolasmazur.github.io/day-schedule/" target="_blank">Day Scheduler</a></header>
                 <a href="https://nikolasmazur.github.io/day-schedule/" target="_blank"><img src={SmallOne} alt="Day Scheduler web application." /></a>
                 <h4 className="repository">
-                <a href="https://github.com/NikolasMazur/day-schedule" target="_blank">Repository</a>
-                <br />
-                Created with: HTML, CSS, JavaScript
-                </h4>
+                  <a href="https://github.com/NikolasMazur/day-schedule" target="_blank">Repository</a>
+                  <br />
+                  Created with: HTML, CSS, JavaScript
+                  </h4>
             </section>
             {/* <!-- Project 3 --> */}
             <section className="project-card">
               <header className="flex"><a href="https://github.com/NikolasMazur/Command-Line-Employee-Tracker" target="_blank">Employee Tracker</a></header>
                 <a href="https://github.com/NikolasMazur/Command-Line-Employee-Tracker" target="_blank"><img src={SmallTwo} alt="Command Line Employee Tracker." /></a>
                 <h4 className="repository">
-                <a href="https://github.com/NikolasMazur/Command-Line-Employee-Tracker" target="_blank">Repository</a>
-                <br />
-                Created with: JavaScript, NodeJS, InquirerJS
-                </h4>
+                  <a href="https://github.com/NikolasMazur/Command-Line-Employee-Tracker" target="_blank">Repository</a>
+                  <br />
+                  Created with: JavaScript, NodeJS, InquirerJS
+                  </h4>
             </section>
             {/* <!-- Project 4 --> */}
             <section className="project-card">
               <header className="flex"><a href="https://nikolasmazur.github.io/password-generator/" target="_blank">Password Generator</a></header>
                 <a href="https://nikolasmazur.github.io/password-generator/" target="_blank"><img src={SmallThree} alt="Random Password Generator web application" /></a>
                 <h4 className="repository">
-                <a href="https://github.com/NikolasMazur/password-generator" target="_blank">Repository</a>
-                <br />
-                Created with: HTML, CSS, JavaScript
+                  <a href="https://github.com/NikolasMazur/password-generator" target="_blank">Repository</a>
+                  <br />
+                  Created with: HTML, CSS, JavaScript
                 </h4>
             </section>
             {/* <!-- Project 5 --> */}
@@ -55,9 +55,9 @@ function Project() {
               <header className="flex"><a href="https://nikolasmazur.github.io/password-generator/" target="_blank">Rent a Dev</a></header>
                 <a href="https://nikolasmazur.github.io/password-generator/" target="_blank"><img src={MediumOne} alt="No current photo" /></a>
                 <h4 className="repository">
-                <a href="https://github.com/anitachengalva/rent-a-dev" target="_blank">Repository</a>
-                <br />
-                Created with: React, MongoDB, Semantic UI, GraphQL, NodeJS
+                  <a href="https://github.com/anitachengalva/rent-a-dev" target="_blank">Repository</a>
+                  <br />
+                  Created with: React, MongoDB, Semantic UI, GraphQL, NodeJS
                 </h4>
             </section>
             {/* <!-- Project 6 --> */}
@@ -65,9 +65,9 @@ function Project() {
               <header className="flex"><a href="https://nikolasmazur.github.io/password-generator/" target="_blank">Busy Parents</a></header>
                 <a href="https://nikolasmazur.github.io/password-generator/" target="_blank"><img src={MediumTwo} alt="Random Password Generator web application" /></a>
                 <h4 className="repository">
-                <a href="https://github.com/crumwj22/busy-parents" target="_blank">Repository</a>
-                <br />
-                Created with: NodeJS, Handlebars, mySQL, Animate.css
+                  <a href="https://github.com/crumwj22/busy-parents" target="_blank">Repository</a>
+                  <br />
+                  Created with: NodeJS, Handlebars, mySQL, Animate.css
                 </h4>
             </section>
           </main>
