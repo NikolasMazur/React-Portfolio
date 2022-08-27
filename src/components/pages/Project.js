856c4a314e358489af4b5e9568f6d11ebc506877
Nikolas <kolemazur@gmail.com>
2022-08-27 11:45:51 -0600

    Change displayed projects

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -2,8 +2,8 @@ import React from 'react';
 import MainProject from '../../assets/images/pocket-monster.png'
 import SmallOne from '../../assets/images/day-scheduler.png'
 import SmallTwo from '../../assets/images/inquirer.PNG'
-import SmallThree from '../../assets/images/password-generator.png'
-import MediumOne from '../../assets/images/password-generator.png'
+import SmallThree from '../../assets/images/social-network-api.png'
+import MediumOne from '../../assets/images/jate.png'
 import MediumTwo from '../../assets/images/busy-parents-dashboard.png'
 
 function Project() {
@@ -11,8 +11,9 @@ function Project() {
         <div>
           <main>
             {/* <!-- Main Project --> */}
-            <section id="main-project">
-              <header className="flex"><a href="https://areitan.github.io/PocketMonster/" target="_blank">PocketMonster</a></header>
+            {/* <section id="main-project"> */}
+            <section className="project-card">
+              <header className="flex"><a href="https://areitan.github.io/PocketMonster/" target="_blank">Pocket Monster</a></header>
                 <a href="https://areitan.github.io/PocketMonster/" target="_blank"><img src={MainProject} alt="PocketMonster landing page." /></a>
                 <h4 className="repository">
                   <a href="https://github.com/areitan/PocketMonster" target="_blank">Repository</a>
@@ -42,28 +43,28 @@ function Project() {
             </section>
             {/* <!-- Project 4 --> */}
             <section className="project-card">
-              <header className="flex"><a href="https://nikolasmazur.github.io/password-generator/" target="_blank">Password Generator</a></header>
-                <a href="https://nikolasmazur.github.io/password-generator/" target="_blank"><img src={SmallThree} alt="Random Password Generator web application" /></a>
+              <header className="flex"><a href="https://github.com/NikolasMazur/MongoDB-Social-Network-API" target="_blank">MongoDB API</a></header>
+                <a href="https://github.com/NikolasMazur/MongoDB-Social-Network-API" target="_blank"><img src={SmallThree} alt="MongoDB Social Network API" /></a>
                 <h4 className="repository">
-                  <a href="https://github.com/NikolasMazur/password-generator" target="_blank">Repository</a>
+                  <a href="https://github.com/NikolasMazur/MongoDB-Social-Network-API" target="_blank">Repository</a>
                   <br />
-                  Created with: HTML, CSS, JavaScript
+                  Created with: JavaScript, NodeJS, MongoDB, Mongoose
                 </h4>
             </section>
             {/* <!-- Project 5 --> */}
             <section className="project-card">
-              <header className="flex"><a href="https://nikolasmazur.github.io/password-generator/" target="_blank">Rent a Dev</a></header>
-                <a href="https://nikolasmazur.github.io/password-generator/" target="_blank"><img src={MediumOne} alt="No current photo" /></a>
+              <header className="flex"><a href="https://github.com/NikolasMazur/Text-Editor" target="_blank">Text Editor</a></header>
+                <a href="https://github.com/NikolasMazur/Text-Editor" target="_blank"><img src={MediumOne} alt="No current photo" /></a>
                 <h4 className="repository">
-                  <a href="https://github.com/anitachengalva/rent-a-dev" target="_blank">Repository</a>
+                  <a href="https://nm-text-editor.herokuapp.com/" target="_blank">Repository</a>
                   <br />
-                  Created with: React, MongoDB, Semantic UI, GraphQL, NodeJS
+                  Created with: JavaScript, NodeJS, Webpack, Bable
                 </h4>
             </section>
             {/* <!-- Project 6 --> */}
             <section className="project-card">
-              <header className="flex"><a href="https://nikolasmazur.github.io/password-generator/" target="_blank">Busy Parents</a></header>
-                <a href="https://nikolasmazur.github.io/password-generator/" target="_blank"><img src={MediumTwo} alt="Random Password Generator web application" /></a>
+              <header className="flex"><a href="https://busy-parents-jesse.herokuapp.com/" target="_blank">Busy Parents</a></header>
+                <a href="https://busy-parents-jesse.herokuapp.com/" target="_blank"><img src={MediumTwo} alt="Random Password Generator web application" /></a>
                 <h4 className="repository">
                   <a href="https://github.com/crumwj22/busy-parents" target="_blank">Repository</a>
                   <br />
