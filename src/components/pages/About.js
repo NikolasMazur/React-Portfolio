eef0b4eb087efb09e2808bc0bf941ca4c148755b
Nikolas <kolemazur@gmail.com>
2022-08-27 11:55:44 -0600

    Rewrite first paragraph

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -13,12 +13,19 @@ export default function About() {
          </header>
           <section id="about-me">
             {/* <img className="photo" src={Photo} alt="Out of date Nikolas picture" /> */}
-            <p className="paragraph"> a developer currently located in Lander, Wyoming. I've received certifications in varous languages including HTML, CSS, SQL, Python, and MongoDB. Currently I'm enrolled in a full 
-              stack development bootcamp to further my knowledge of web devlopment languages and technologies; namely HTML, CSS, and JavaScript.
+            <p className="paragraph"> 
+              a full-stack web developer located in Lander, Wyoming. I am a recent 
+              University of Washington full stack web development bootcamp 
+              graduate with experience in IT support. Additionally, I've received 
+              various certifications in varous languages including HTML, CSS, SQL, 
+              Python, and MongoDB.
               <br />
-              From 2021 to 2022 I was employed by Falcon 
-              Trading Systems as a systems technician. From 2017 to 2019 I was a student at Central Wyoming College studying film, my time there 
-              fostered my love of the visual arts leading to making various short films with friends and former classmates.
+              <br />
+              From 2021 to 2022 I was employed by Falcon Trading Systems as a 
+              systems technician. From 2017 to 2019 I was a student at Central 
+              Wyoming College studying film, my time there fostered my love of the 
+              visual arts leading to making various short films with friends and 
+              former classmates.
             </p>
           </section>
         </div>
