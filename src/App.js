77d522383bdb74580eb1dafe5b20bbbd593ad67f
Nikolas <kolemazur@gmail.com>
2023-02-06 00:29:21 -0700

    Fix navigation routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import PortfolioContainer from "./components/PortfolioContainer";
+import Homepage from './components/pages/Homepage'
+import Project from './components/pages/Project'
+import About from './components/pages/About'
+import Resume from './components/pages/Resume'
 
 // const App = () => <Project />;
 
 function App() {
     return(
         <div>
-            <PortfolioContainer />
+            <BrowserRouter>
+                {/* <Homepage /> */}
+                <Routes>
+                    <Route exact path='/React-Portfolio' element={<Homepage />} />
+                    <Route path='/Project' element={<Project />} />
+                    <Route path='/About' element={<About />} />
+                    <Route path='/Resume' element={<Resume />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,44 +1,21 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 
-function Nav({currentPage, handlePageChange}) {
-  // const handlePageChange = (page) => {
-  //   console.log(page)
-  //   // setCurrentPage(page);
-  // }
+function Nav() {
   return (
     <div>
     <nav className="links nav-link">
       <ul className="navigation">
-        <li><a 
-              href="#project"
-              onClick={() => handlePageChange('Project')}
-              className={currentPage === 'Project' ? 'nav-link active' : 'links'}
-              >Development
-            </a>
+        <li>
+          <NavLink to="/about">
+            About
+          </NavLink>
         </li>
         <br />
-        {/* <li><a 
-              href="#contact"
-              onClick={() => handlePageChange('Contact')}
-              className={currentPage === 'Contact' ? 'nav-link active' : 'links'}
-              >Contact
-            </a>
-        </li>
-        <br /> */}
-        <li><a 
-              href="#resume"
-              onClick={() => handlePageChange('Resume')}
-              className={currentPage === 'Resume' ? 'nav-link active' : 'links'}
-              >Resume
-            </a>
-        </li>
-        <br />
-        <li><a 
-              href="#about"
-              onClick={() => handlePageChange('About')}
-              className={currentPage === 'About' ? 'nav-link active' : 'links'}
-              >About
-            </a>
+        <li>
+          <NavLink to="/React-Portfolio">
+              Home
+          </NavLink>
         </li>
       </ul>
     </nav>
